83bace071356789abac7c58f9a8db479e1b58eaf
Ozan Tekin <ardilozantekin@gmail.com>
2025-04-10 15:24:31 +0300

    style: update product details og image

--- app/(marketing)/products/[slug]/opengraph-image.tsx
+++ app/(marketing)/products/[slug]/opengraph-image.tsx
@@ -1,4 +1,3 @@
-import { createImageUrl } from "@shsfwork/lib/content-collections/images";
 import { cn } from "@shsfwork/lib/cn";
 import { ImageResponse } from "@vercel/og";
 import { allProducts, type Product } from "content-collections";
@@ -20,11 +19,11 @@ export const contentType = "image/png";
 export default async function Image({ params }: Props) {
   const slug = params.slug;
   // @ts-expect-error - allProducts is imported from content collections and not recognized by TypeScript
-  const post: Product = allProducts.find((p) => slug === p._meta.path);
-  if (!post) {
+  const products: Product = allProducts.find((p) => slug === p._meta.path);
+  if (!products) {
     return new Response(
       JSON.stringify({
-        message: "Could not find post with slug: " + slug,
+        message: "Could not find products with slug: " + slug,
       }),
       {
         status: 404,
@@ -45,16 +44,16 @@ export default async function Image({ params }: Props) {
   return new ImageResponse(
     (
       <div
-        tw="w-full h-full p-4 flex bg-orange-500"
+        tw="w-full h-full p-4 flex bg-indigo-500"
         style={{
-          backgroundImage: "linear-gradient(to right, #0B7AFF, #59B9FF)",
+          backgroundImage: "linear-gradient(to right, #312e81, #581c87)",
         }}
       >
         <div tw="rounded-xl w-full h-full p-4 flex bg-white shadow-lg">
           <img
             width="475"
             height="562"
-            src={createImageUrl(post.image.url, 475, 562)}
+            src={products.og}
             tw="rounded-xl shadow-lg"
             alt="Product Post Image"
             style={{ objectFit: "cover" }}
@@ -64,33 +63,33 @@ export default async function Image({ params }: Props) {
               <img
                 width={48}
                 height={48}
-                alt={SITE.title}
-                src="https://avatars.githubusercontent.com/u/168901475?s=200&v=4"
+                alt={products.title}
+                src={products.icon}
                 tw="rounded-lg"
                 style={{ objectFit: "cover" }}
               />
               <span
                 tw={cn("font-bold text-zinc-950 my-4", {
-                  "text-6xl": post.title.length < 40,
-                  "text-5xl": post.title.length >= 40,
+                  "text-6xl": products.title.length < 40,
+                  "text-5xl": products.title.length >= 40,
                 })}
               >
-                {post.title}
+                {products.title}
               </span>
               <span
                 tw={cn("text-zinc-600", {
-                  "text-4xl": post.title.length < 48,
-                  "text-3xl": post.title.length >= 48,
+                  "text-4xl": products.title.length < 48,
+                  "text-3xl": products.title.length >= 48,
                 })}
               >
-                {post.excerpt}
+                {products.excerpt}
               </span>
             </div>
             <div tw="flex justify-between items-end w-full text-zinc-500 mt-4">
-              <span className="text-xl ">{post.readingTime}</span>
-              <span className="text-xl ">{formatDate(post.createdAt)}</span>
+              <span className="text-xl ">{products.readingTime}</span>
+              <span className="text-xl ">{formatDate(products.createdAt)}</span>
             </div>
-            <p tw="absolute text-[#0B7AFF] bottom-4 right-4 text-xl">
+            <p tw="absolute text-[#312e81] bottom-4 right-4 text-xl">
               {SITE.domain}
             </p>
           </div>
